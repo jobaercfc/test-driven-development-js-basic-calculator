86bf2b1ad593435cd0b14e045b12f5de636c3b2d
Abdullah Al Jobaer <surid@Abdullahs-MacBook-Air.local>
2020-02-07 14:06:44 +0900

    Test case attached

--- test.js
+++ test.js
@@ -24,11 +24,11 @@ it('should return '+divide, () => {
 
 //validate numbers
 it('should return false if any string is used', () => {
-    assert.equal(operations.validateNumbers(num1, num2), false);
+    assert.equal(operations.validateNumbers('samme', num2), false);
 });
 
 it('should return false if any string is used', () => {
-    assert.equal(operations.validateNumbers(num1, num2), false);
+    assert.equal(operations.validateNumbers('sada', 'ss'), false);
 });
 
 it('should return true if both are numbers', () => {
