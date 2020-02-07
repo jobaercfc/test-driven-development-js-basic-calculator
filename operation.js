7275095cccd0609974333066d8932bc68a5f5ee1
Abdullah Al Jobaer <surid@Abdullahs-MacBook-Air.local>
2020-02-07 13:50:34 +0900

    Test case attached

--- operation.js
+++ operation.js
@@ -1,6 +1,13 @@
 const add = (x, y) => x + y;
 const subtract = (x, y) => (+x) - (+y);
 const multiply = (x, y) => (+x) * (+y);
-const divides = (x, y) => (+x) / (+y);
+const divides = (x, y) => parseInt((x) / (y));
 
-module.exports = {add, subtract, multiply, divides};
+const validateNumbers = (x, y) => {
+    if (isNaN(x) || isNaN(y)) {
+      return false
+    }
+    return true
+  }
+
+module.exports = {add, subtract, multiply, divides, validateNumbers};
--- test.js
+++ test.js
@@ -2,8 +2,35 @@ const assert = require('assert');
 const operations = require('./operation.js')
 
 var num1 = 5, num2 = 6;
-var sum = 12;
+var sum = 11;
+var sub = -1;
+var multiply = 30, divide = 0;
 
 it('should return '+sum, () => {
     assert.equal(operations.add(num1,num2), sum);
-})
+});
+
+it('should return '+sub, () => {
+    assert.equal(operations.subtract(num1,num2), sub);
+});
+
+it('should return '+multiply, () => {
+    assert.equal(operations.multiply(num1,num2), multiply);
+});
+
+it('should return '+divide, () => {
+    assert.equal(operations.divides(num1,num2), divide);
+});
+
+//validate numbers
+it('should return false if any string is used', () => {
+    assert.equal(operations.validateNumbers(num1, num2), false);
+});
+
+it('should return false if any string is used', () => {
+    assert.equal(operations.validateNumbers(num1, num2), false);
+});
+
+it('should return true if both are numbers', () => {
+    assert.equal(operations.validateNumbers(num1, num2), true);
+});
